485c93c8ef8c62df543fece7d7b37c208ddbbec3
965348vivek <vivek.h.vishwakarma@gmail.com>
2025-05-25 00:13:43 +0530

    Create AtmInterface.java

---
+++ AtmInterface.java
@@ -0,0 +1,8 @@
+package Atm;
+
+public interface AtmInterface {
+public void viewBalance();
+public void withdrawAmount(double withdrawAmount);
+public void depositAount (double depositeAmount);
+public void viewMiniStatement();
+}
